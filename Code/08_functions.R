161a3a1ccb387f58c4e9b09dbe5cfbc50bef2bc2
juliapapa01 <papa.julia.maria@gmail.com>
2026-04-23 15:43:41 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,11 @@
+#my functions
+
+somma=function(x,y) {   #x e y sono gli argomenti
+  z=x+y           
+  return(z)        #output
+}
+
+differenza=function(x,y) {   
+  z=x-y           
+  return(z)       
+}
