7cf6ca921caeebf4736b1659b16f19a5c48c41ec
juliapapa01 <papa.julia.maria@gmail.com>
2026-04-29 17:04:06 +0200

    Update 08_functions.R

--- Code/08_functions.R
+++ Code/08_functions.R
@@ -9,3 +9,58 @@ differenza=function(x,y) {
   z=x-y           
   return(z)       
 }
+
+#par(mfrow=c(1,2)) with default (1,2) (otherwise just function(nx,ny))
+mf=function(nx=1, ny=2){
+  par(mfrow=c(nx, ny))
+  }
+
+#struttura if else
+numeri=function (x){
+  if(x>0){
+    print("positivo?")
+    }
+  else{
+    print("negativo")
+    }
+  }
+
+numeri=function (x){
+  if(x>0){
+    print("positivo?")
+    }
+  else if(x<0){
+    print("negativo")
+    }
+    else{
+      print("zero")
+      }
+  }
+
+#for loop
+loop=function(){
+  for(i in 1:10){
+    print(i)
+    }
+  }
+loop()
+
+loop2=function(){
+  for(i in 1:10){
+    op=i*2
+    print(op)
+    }
+  }
+
+loop3=function(){
+  for(i in 1:10000){
+    op=i^3*2
+    print(op)
+    }
+  }
+
+
+
+
+
+
